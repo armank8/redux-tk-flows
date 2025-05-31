4a93826a29d7f5d323896380ded23e4bc24877c6
Arman-Kabir <armankabir8@gmail.com>
2025-05-31 11:09:26 +0600

    server connection issue solved

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const connectDB = async () => {
+  console.log("trying db");
   try {
-    await mongoose.connect(process.env.MONGO_URI!);
-    console.log('MongoDB Connected');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
+    const conn = await mongoose.connect('mongodb+srv://armank:armank12@cluster0.y0bza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+      dbName: 'redux-tk-flows',
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
     process.exit(1);
   }
 };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import connectDB from './config/db';
-import authRoutes from './routes/auth.routes';
+import connectDB from './config/db.js';
+import authRoutes from './routes/auth.routes.js';
 
 dotenv.config();
 connectDB();
+console.log("errorrrrrrr");
 
 const app = express();
 app.use(cors());
